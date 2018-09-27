25411d1ae24e0c4a5ac88336353863ddd9b254da
Florian Fida <f+git@fida.biz>
2018-09-28 00:05:45 +0200

    add api test

---
+++ UrlBucket.Tests/ApiClientTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using NUnit.Framework;
+
+namespace UrlBucket.Tests {
+    public class ApiClientTests {
+        private WebApplicationFactory<Startup> _factory;
+
+        public ApiClientTests() {
+            _factory = new WebApplicationFactory<Startup>();
+        }
+
+        private ApiClient GetApiClient() {
+            var httpClient = _factory.CreateClient();
+            return new ApiClient(httpClient.BaseAddress.ToString(), httpClient);
+        }
+
+        [Test]
+        public async Task StoreAndRetrieve() {
+            var client = GetApiClient();
+            var bytes = Encoding.UTF8.GetBytes("Hello World");
+
+            var storeResponse = await client.ApiStoreBytesPostAsync(bytes, "xxx://xxx", "text/plain");
+            var retrievedFile = await client.ApiRetrieveGetAsync("xxx://xxx");
+
+            Assert.AreEqual(StatusCodes.Status200OK, storeResponse.Status);
+            Assert.AreEqual(bytes, retrievedFile.Content);
+        }
+    }
+}
