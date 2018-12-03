fe653f799aaf69b3249d70c2bd1c6d890982650b
Florian Fida <florian.fida@post.at>
2018-12-03 15:10:32 +0100

    size limit

--- UrlBucket.Lib/Services/DownloadService.cs
+++ UrlBucket.Lib/Services/DownloadService.cs
@@ -9,6 +9,13 @@ using UrlBucket.Lib.Models;
 
 namespace UrlBucket.Lib.Services {
     public class DownloadService {
+
+        public DownloadService() {
+            if(!long.TryParse(Environment.GetEnvironmentVariable("SIZE_LIMIT"), out _sizeLimit)) {
+                _sizeLimit = 3_145_728L;
+            }
+        }
+
         public async Task<FileModel> DownloadUrlAsync(Uri url, string userAgent = null) {
             if (url == null) throw new ArgumentNullException(nameof(url));
             if (string.IsNullOrEmpty(userAgent)) userAgent = null;
@@ -17,10 +24,12 @@ namespace UrlBucket.Lib.Services {
             };
 
             using (var wc = new WebClient()) {
+                wc.DownloadProgressChanged += OnDownloadProgressChanged;
                 var ua = userAgent ?? Environment.GetEnvironmentVariable("DOWNLOAD_USER_AGENT");
                 if (!string.IsNullOrWhiteSpace(ua)) {
                     wc.Headers.Add("user-agent", ua);
                 }
+
                 fm.Content = await wc.DownloadDataTaskAsync(url);
                 fm.ContentType = wc.ResponseHeaders["content-type"];
                 if (string.IsNullOrEmpty(fm.ContentType) && fm.Content != null && fm.Content.Length > 1) {
@@ -32,6 +41,21 @@ namespace UrlBucket.Lib.Services {
             }
 
             return fm;
+        }
+
+        private readonly long _sizeLimit;
+
+        private void OnDownloadProgressChanged(object o, DownloadProgressChangedEventArgs ev) {
+            var wc = (WebClient)o;
+            if (ev.TotalBytesToReceive >= _sizeLimit) {
+                Console.WriteLine($"totalSize >= sizeLimit ({ev.TotalBytesToReceive} >= {_sizeLimit})");
+                wc.CancelAsync();
+            }
+
+            if (ev.BytesReceived >= _sizeLimit) {
+                Console.WriteLine($"bytesReceived >= sizeLimit ({ev.BytesReceived} >= {_sizeLimit})");
+                wc.CancelAsync();
+            }
         }
     }
 }
