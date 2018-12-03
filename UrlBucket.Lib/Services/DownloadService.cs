3fc598b14c31f330e1bc241dcd594232f1bbcdf2
Florian Fida <florian.fida@post.at>
2018-12-03 12:30:58 +0100

    content-type sniffing

--- UrlBucket.Lib/Services/DownloadService.cs
+++ UrlBucket.Lib/Services/DownloadService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using MimeDetective;
 using UrlBucket.Lib.Helper;
 using UrlBucket.Lib.Models;
 
@@ -22,6 +23,12 @@ namespace UrlBucket.Lib.Services {
                 }
                 fm.Content = await wc.DownloadDataTaskAsync(url);
                 fm.ContentType = wc.ResponseHeaders["content-type"];
+                if (string.IsNullOrEmpty(fm.ContentType) && fm.Content != null && fm.Content.Length > 1) {
+                    var contentType = MimeTypes.GetFileType(() => fm.Content, null, fm.Content);
+                    if (contentType != null && !string.IsNullOrWhiteSpace(contentType.Mime)) {
+                        fm.ContentType = contentType.Mime;
+                    }
+                }
             }
 
             return fm;
