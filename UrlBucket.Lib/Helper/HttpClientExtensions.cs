2e1035ec0d6f05ca66f2984706eb815abcf9931e
FF <f+git@fida.biz>
2018-12-06 13:37:02 +0100

    custom http request

---
+++ UrlBucket.Lib/Helper/HttpClientExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UrlBucket.Lib.Models;
+
+namespace UrlBucket.Lib.Helper {
+    public static class HttpClientExtensions {
+        public static async Task<BaseFileContentModel> SendAsync(this HttpClient httpClient, HttpMethod method, Uri url, string userAgent = null, long maxSize = 1048576, bool useContentLengthHeader = true, int buffSiz = 8192, CancellationToken ct = default(CancellationToken)) {
+
+            if (!string.IsNullOrWhiteSpace(userAgent)) {
+                httpClient.DefaultRequestHeaders.Add("User-Agent", userAgent);
+            }
+
+            var content = new BaseFileContentModel();
+            var buffer     = new byte[buffSiz];
+            var totalBytes = 0L;
+
+            using (var cts = CancellationTokenSource.CreateLinkedTokenSource(ct)) {
+                    using (var ms = new MemoryStream())
+                    using (var request = new HttpRequestMessage(method, url))
+                    using (var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cts.Token))
+                    using (var responseStream = await response.Content.ReadAsStreamAsync())
+                    {
+                        if (useContentLengthHeader && response.Content.Headers.ContentLength.HasValue && response.Content.Headers.ContentLength.Value > maxSize) {
+                            cts.Cancel();
+                            throw new InvalidOperationException("content length exceeds max size");
+                        }
+
+                        while (!cts.Token.IsCancellationRequested) {
+                            var bytesRead = await responseStream.ReadAsync(buffer, 0, buffer.Length, cts.Token);
+                            if(bytesRead == 0) break;
+                            totalBytes += bytesRead;
+                            if (totalBytes >= maxSize) {
+                                cts.Cancel();
+                                throw new InvalidOperationException("downloaded content exceeds max size");
+                            }
+                            await ms.WriteAsync(buffer, 0, bytesRead, cts.Token);
+                        }
+
+                        content.ContentType = response?.Content?.Headers?.ContentType?.MediaType;
+                        content.Content = ms.ToArray();
+                    }
+            }
+
+            return content;
+        }
+    }
+}
---
+++ UrlBucket.Tests/HttpClientExtensionTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using UrlBucket.Lib.Helper;
+
+namespace UrlBucket.Tests {
+    public class HttpClientExtensionTests {
+
+        private const long DownloadSize1Mb = 1048576;
+
+        [Test]
+        public async Task UserAgent() {
+            var hc = new HttpClient();
+            var r = await hc.SendAsync(HttpMethod.Get, new Uri("http://httpbin.org/get"), "lala/1");
+            var responseText = Encoding.UTF8.GetString(r.Content);
+            TestContext.WriteLine(responseText);
+            Assert.IsTrue(responseText.Contains("\"User-Agent\": \"lala/1\""));
+        }
+
+        [Test]
+        public void ContentLengthSizeLimit() {
+            var hc = new HttpClient();
+            Assert.Throws<InvalidOperationException>(() => {
+                hc.SendAsync(HttpMethod.Get, new Uri("http://speed.hetzner.de/10GB.bin"), null, DownloadSize1Mb, true).GetAwaiter().GetResult();
+            });
+        }
+
+        [Test]
+        public void DownloadSizeLimit() {
+            var hc = new HttpClient();
+            Assert.Throws<InvalidOperationException>(() => {
+                hc.SendAsync(HttpMethod.Get, new Uri("http://speed.hetzner.de/10GB.bin"), null, DownloadSize1Mb, false).GetAwaiter().GetResult();
+            });
+        }
+
+        [Test]
+        public async Task Timeout() {
+            var hc = new HttpClient();
+            var cts = new CancellationTokenSource(1);
+            var cnt = 0;
+            try {
+                await hc.SendAsync(HttpMethod.Get, new Uri("http://speed.hetzner.de/10GB.bin"), null, long.MaxValue, ct: cts.Token);
+            } catch (TaskCanceledException) {
+                cnt++;
+            } catch (OperationCanceledException) {
+                cnt++;
+            }
+
+            Assert.AreEqual(1,cnt);
+        }
+    }
+}
