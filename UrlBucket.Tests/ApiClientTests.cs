2b429f234e0fb44fd4ebd9acc428e9f7d56936c9
f <>
2019-01-23 13:52:57 +0100

    move api client to own proj

--- UrlBucket.Tests/ApiClientTests.cs
+++ UrlBucket.Tests/ApiClientTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
 using NUnit.Framework;
+using UrlBucket.Client;
 
 namespace UrlBucket.Tests {
     public class ApiClientTests {
